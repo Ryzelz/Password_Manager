5ea18c222ab4f798ea28259e667e43739465d270
Ryzel <100330023+Ryzelz@users.noreply.github.com>
2023-04-15 19:34:56 +0800

    changed the code into OOP
    big overhall of my funky code

---
+++ Main_execution.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  9 06:13:09 2023
+
+@author: ryzel
+"""
+
+#Main execution menu
+from CRUD_module import PasswordManager
+
+# Create an instance of PasswordManager
+password_manager = PasswordManager('PASSDB.db')
+
+while True:
+    print("Password manager CLI")
+    menu_cursor = input("\n [a] Create new password \n [b] Display all passwords \n [c] Delete pass \n [d] Change pass(WIP) \n [z] Exit \n Input: ")
+    menu_cursor = menu_cursor.lower()
+    if menu_cursor == "a":
+        password_manager.create_new_pass()
+    elif menu_cursor == "b":
+        password_manager.display_users()
+    elif menu_cursor == "c":
+        password_manager.display_users()
+        uuid_account_deletion = input("Input your desired uuid: ")
+        password_manager.delete_pass(uuid_account_deletion)
+    elif menu_cursor == "d":
+        # Implement the logic for changing password
+        pass
+    elif menu_cursor == "z":
+        password_manager.close_connection()
+        break
